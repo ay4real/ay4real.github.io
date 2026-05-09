5280330e47eefb213c6790b66f32075db89ea0f5
Ayomide Ogbede <ayomide.ogbede@barnksfortetech.com>
2026-05-09 18:26:35 +0100

    Update metadata title to include credentials

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Ayomide Ogbede",
+  title: "Ayomide Ogbede, C.itp, MCPN",
   description: "Ayomide Ogbede's Portfolio",
   generator: "Ay4real",
 };
